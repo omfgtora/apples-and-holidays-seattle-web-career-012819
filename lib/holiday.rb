bd7905b99d38bde6d09ada6c06c3f488cd7c97de
Ethan Roberts <ejrobe@gmail.com>
2019-01-09 00:25:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  puts season
-  puts holiday_name
+  holiday_hash[season][holiday_name] << supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
