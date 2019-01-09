8972d58ebbc50f74d492ddd0f18f62cdd445e909
Ethan Roberts <ejrobe@gmail.com>
2019-01-09 02:00:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |k,v|
     puts "#{k.capitalize}:"
     v.each do |k,v|
-      puts "  #{k.split(' ').capitalize}: #{v.join(', ')}"
+      puts "  #{k.split('_').capitalize}: #{v.join(', ')}"
     end
   end
 end
