584f1da7dbf8db27705d857d9b972277272e5302
Ethan Roberts <ejrobe@gmail.com>
2019-01-09 01:52:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |k,v|
-    puts "#{k}:"
+    puts "#{k.capitalize}:"
     v.each do |k,v|
       puts "#{k}: #{v}"
     end
